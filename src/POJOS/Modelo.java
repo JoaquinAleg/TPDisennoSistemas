626da4223000066b89edd7e8388baa2a9ade4ae0
José Bracalenti <josebracalenti@hotmail.com>
2023-11-20 20:42:29 -0300

    agregado fabricado en modelo

--- src/POJOS/Modelo.java
+++ src/POJOS/Modelo.java
@@ -1,4 +1,6 @@
 package POJOS;
+import java.util.List;
+
 import jakarta.persistence.*;
 
 @Entity
@@ -7,38 +9,82 @@ public class Modelo {
 	@Id
 	@Column
 	private long idModelo;
-	@Column
-	private long idMarca;
+	@ManyToOne
+	@JoinColumn(name = "idMarca", nullable = false, referencedColumnName = "idMarca", 
+	foreignKey=@ForeignKey(name = "fk_ReciboCuota", value = ConstraintMode.CONSTRAINT))
+	private Marca Marca;
 	@Column
 	private long valorPorcentual;
 	@Column
 	private String nombreModelo;
+	@OneToMany
+	@JoinColumn(name = "idModelo", nullable = false, referencedColumnName = "idModelo", 
+	foreignKey=@ForeignKey(name = "fk_ReciboCuota", value = ConstraintMode.CONSTRAINT))
+	private List<fabricado> anioFabricacion;
+	
 	
 	public Modelo() {}
 
-	public Modelo(long idModelo, long idMarca, long valorPorcentual, String nombreModelo) {
+	
+
+	
+
+
+	public Modelo(long idModelo, POJOS.Marca marca, long valorPorcentual, String nombreModelo,
+			List<fabricado> anioFabricacion) {
 		super();
 		this.idModelo = idModelo;
-		this.idMarca = idMarca;
+		Marca = marca;
 		this.valorPorcentual = valorPorcentual;
 		this.nombreModelo = nombreModelo;
+		this.anioFabricacion = anioFabricacion;
 	}
+
+
+
+
+
+
+	public List<fabricado> getAnioFabricacion() {
+		return anioFabricacion;
+	}
+
+
+
+
+
+
+	public void setAnioFabricacion(List<fabricado> anioFabricacion) {
+		this.anioFabricacion = anioFabricacion;
+	}
+
+
+
+
+
+
+	public Marca getMarca() {
+		return Marca;
+	}
+
+
+
+	public void setMarca(Marca marca) {
+		Marca = marca;
+	}
+
+
 
 	public long getIdModelo() {
 		return idModelo;
 	}
 
+
+
 	public void setIdModelo(long idModelo) {
 		this.idModelo = idModelo;
 	}
 
-	public long getIdMarca() {
-		return idMarca;
-	}
-
-	public void setIdMarca(long idMarca) {
-		this.idMarca = idMarca;
-	}
 
 	public long getValorPorcentual() {
 		return valorPorcentual;
