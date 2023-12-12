733e3a68580c84952df6e7a1e20f9b921e5d478c
JoaquinAleg <joaquin.alegre2000@gmail.com>
2023-12-11 21:49:05 -0300

    Comencé con un mensaje que no teníamos hecho

--- src/pantalla/BuscarCliente.java
+++ src/pantalla/BuscarCliente.java
@@ -16,6 +16,9 @@ import java.awt.GridBagConstraints;
 import java.awt.Insets;
 import javax.swing.border.LineBorder;
 import javax.swing.table.DefaultTableModel;
+
+import com.toedter.calendar.JDateChooser;
+
 import javax.swing.ListSelectionModel;
 import javax.swing.event.ListSelectionEvent;
 import javax.swing.event.ListSelectionListener;
@@ -33,7 +36,8 @@ import javax.swing.JComboBox;
 import java.awt.SystemColor;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
+import java.time.ZoneId;
+import java.util.Date;
 import java.util.List;
 
 import javax.swing.border.MatteBorder;
@@ -348,6 +352,8 @@ public class BuscarCliente extends JFrame {
 		gbl_panel_2.columnWeights = new double[]{0.0, 1.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		gbl_panel_2.rowWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
 		panel_2.setLayout(gbl_panel_2);
+		JPanel panelCliente = new JPanel();
+		
 		
 		JButton Boton_Cancelar = new JButton("Cancelar");
 		Boton_Cancelar.addActionListener(new ActionListener() {
@@ -376,12 +382,13 @@ public class BuscarCliente extends JFrame {
 			lblNewLabel_5_1.setBorder(null);
 			panel_3.add(lblNewLabel_5_1);
 			panel_3.setBorder(new MatteBorder(0, 0, 4, 0, (Color) new Color(0, 0, 0)));
-			Boton_Continuar_1.setText("Continuar\r\n");
+			Boton_Continuar_1.setText("Continuar");
 			Boton_Continuar_1.addActionListener(new ActionListener() {
 				public void actionPerformed(ActionEvent e) { 
 	
 	                if (cliDTO != null) {
-	                    
+	                	cargarPanelCliente(panelCliente, cliDTO);
+	                	JOptionPane.showMessageDialog(null, panelCliente);
 						CrearPoliza_1 FuturaPantalla = new CrearPoliza_1(gestorPoliza, gestorCliente, BuscarCliente.this, cliDTO);
 						try {
 							FuturaPantalla.setVisible(true);
@@ -490,5 +497,137 @@ public class BuscarCliente extends JFrame {
 	}
 	}
     
+	private void cargarPanelCliente( JPanel panelPrincipal, ClienteDTO cliDTO) {
+		
+		String tipoDocumentoCliente = cliDTO.getIdTipoDocumento().toString();
+		String apellidoCliente = cliDTO.getApellido().toString();
+		String nombreCliente = cliDTO.getNombre().toString();
+		String documentoCliente = cliDTO.getNumeroDocumento().toString();
+		String numeroCliente = cliDTO.getNumeroCliente().toString();
+		
+		contentPane = panelPrincipal;
+		contentPane.setBackground(SystemColor.activeCaption);
+		contentPane.setForeground(new Color(0, 0, 0));
+		contentPane.setBorder(new EmptyBorder(0, 0, 0, 0));
+
+		setContentPane(contentPane);
+		GridBagLayout gbl_contentPane = new GridBagLayout();
+		gbl_contentPane.columnWidths = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{202, 0};
+		gbl_contentPane.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		contentPane.setLayout(gbl_contentPane);
+		
+		JPanel panel = new JPanel();
+		panel.setBackground(SystemColor.inactiveCaptionBorder);
+		panel.setBorder(new LineBorder(new Color(0, 0, 0), 4));
+		GridBagConstraints gbc_panel = new GridBagConstraints();
+		gbc_panel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_panel.gridx = 0;
+		gbc_panel.gridy = 0;
+		contentPane.add(panel, gbc_panel);
+		GridBagLayout gbl_panel = new GridBagLayout();
+		gbl_panel.columnWidths = new int[]{0, 0};
+		gbl_panel.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0, 0, 0};
+		gbl_panel.columnWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panel.rowWeights = new double[]{0.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, Double.MIN_VALUE};
+		panel.setLayout(gbl_panel);
+		
+		JPanel panel_3 = new JPanel();
+		panel_3.setBackground(SystemColor.inactiveCaptionBorder);
+		panel_3.setBorder(new MatteBorder(0, 0, 4, 0, (Color) new Color(0, 0, 0)));
+		GridBagConstraints gbc_panel_3 = new GridBagConstraints();
+		gbc_panel_3.fill = GridBagConstraints.BOTH;
+		gbc_panel_3.gridx = 0;
+		gbc_panel_3.gridy = 0;
+		panel.add(panel_3, gbc_panel_3);
+		
+		JLabel lblNewLabel_5_1 = new JLabel("Cliente Seleccionado");
+		lblNewLabel_5_1.setVerticalAlignment(SwingConstants.TOP);
+		lblNewLabel_5_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_5_1.setFont(new Font("Tahoma", Font.PLAIN, 50));
+		lblNewLabel_5_1.setBorder(null);
+		panel_3.add(lblNewLabel_5_1);
+		
+		JPanel prueba = new JPanel();
+		prueba.setBorder(new MatteBorder(2, 0, 0, 0, (Color) new Color(0, 0, 0)));
+		prueba.setBackground(SystemColor.inactiveCaptionBorder);
+		GridBagConstraints gbc_prueba = new GridBagConstraints();
+		gbc_prueba.fill = GridBagConstraints.BOTH;
+		gbc_prueba.gridx = 0;
+		gbc_prueba.gridy = 1;
+		panel.add(prueba, gbc_prueba);
+		GridBagLayout gbl_prueba = new GridBagLayout();
+		gbl_prueba.columnWidths = new int[]{0, 0, 0, 0, 0, 0};
+		gbl_prueba.rowHeights = new int[]{0, 0, 0, 0, 0, 0, 0};
+		gbl_prueba.columnWeights = new double[]{1.0, 1.0, 0.0, 1.0, 1.0, Double.MIN_VALUE};
+		gbl_prueba.rowWeights = new double[]{1.0, 1.0, 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE};
+		prueba.setLayout(gbl_prueba);
+		
+		JLabel lblFechaDeNacimiento_1_1 = new JLabel("Apellido: ");
+		lblFechaDeNacimiento_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblFechaDeNacimiento_1_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		GridBagConstraints gbc_lblFechaDeNacimiento_1_1 = new GridBagConstraints();
+		gbc_lblFechaDeNacimiento_1_1.anchor = GridBagConstraints.WEST;
+		gbc_lblFechaDeNacimiento_1_1.insets = new Insets(30, 50, 5, 5);
+		gbc_lblFechaDeNacimiento_1_1.gridx = 0;
+		gbc_lblFechaDeNacimiento_1_1.gridy = 0;
+		prueba.add(lblFechaDeNacimiento_1_1, gbc_lblFechaDeNacimiento_1_1);
+		JTextField apellidoClienteTP = new JTextField(apellidoCliente);
+		apellidoClienteTP.setEnabled(false);
+		apellidoClienteTP.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		apellidoClienteTP.setBackground(SystemColor.inactiveCaptionBorder);
+		GridBagConstraints gbc_apellidoClienteTP = new GridBagConstraints();
+		gbc_apellidoClienteTP.fill = GridBagConstraints.HORIZONTAL;
+		gbc_apellidoClienteTP.insets = new Insets(30, 0, 5, 40);
+		gbc_apellidoClienteTP.gridx = 1;
+		gbc_apellidoClienteTP.gridy = 0;
+		prueba.add(apellidoClienteTP, gbc_apellidoClienteTP);
+		
+		
+		JLabel lblNewLabel_1_2_1_1 = new JLabel("Nombre: ");
+		lblNewLabel_1_2_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblNewLabel_1_2_1_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		GridBagConstraints gbc_lblNewLabel_1_2_1_1 = new GridBagConstraints();
+		gbc_lblNewLabel_1_2_1_1.anchor = GridBagConstraints.WEST;
+		gbc_lblNewLabel_1_2_1_1.insets = new Insets(30, 0, 5, 10);
+		gbc_lblNewLabel_1_2_1_1.gridx = 3;
+		gbc_lblNewLabel_1_2_1_1.gridy = 0;
+		prueba.add(lblNewLabel_1_2_1_1, gbc_lblNewLabel_1_2_1_1);
+		
+		
+		JTextField nombreClienteLabel = new JTextField(nombreCliente);
+		nombreClienteLabel.setEnabled(false);
+		nombreClienteLabel.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		nombreClienteLabel.setBackground(SystemColor.inactiveCaptionBorder);
+		GridBagConstraints gbc_nombreClienteLabel = new GridBagConstraints();
+		gbc_nombreClienteLabel.fill = GridBagConstraints.HORIZONTAL;
+		gbc_nombreClienteLabel.insets = new Insets(30, 0, 5, 40);
+		gbc_nombreClienteLabel.gridx = 4;
+		gbc_nombreClienteLabel.gridy = 0;
+		prueba.add(nombreClienteLabel, gbc_nombreClienteLabel);
+		
+		JLabel lblEstadoCivil_1_1 = new JLabel("Tipo Documento: ");
+		lblEstadoCivil_1_1.setHorizontalAlignment(SwingConstants.LEFT);
+		lblEstadoCivil_1_1.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		GridBagConstraints gbc_lblEstadoCivil_1_1 = new GridBagConstraints();
+		gbc_lblEstadoCivil_1_1.anchor = GridBagConstraints.WEST;
+		gbc_lblEstadoCivil_1_1.insets = new Insets(20, 50, 40, 5);
+		gbc_lblEstadoCivil_1_1.gridx = 0;
+		gbc_lblEstadoCivil_1_1.gridy = 2;
+		prueba.add(lblEstadoCivil_1_1, gbc_lblEstadoCivil_1_1);
+		
+		JTextField JTextFieldtipoDocumentoCliente = new JTextField(tipoDocumentoCliente);
+		JTextFieldtipoDocumentoCliente.setEnabled(false);
+		JTextFieldtipoDocumentoCliente.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		JTextFieldtipoDocumentoCliente.setBackground(SystemColor.inactiveCaptionBorder);
+		GridBagConstraints gbc_JTextFieldtipoDocumentoCliente = new GridBagConstraints();
+		gbc_JTextFieldtipoDocumentoCliente.fill = GridBagConstraints.HORIZONTAL;
+		gbc_JTextFieldtipoDocumentoCliente.insets = new Insets(20, 0, 40, 30);
+		gbc_JTextFieldtipoDocumentoCliente.gridx = 1;
+		gbc_JTextFieldtipoDocumentoCliente.gridy = 2;
+		prueba.add(JTextFieldtipoDocumentoCliente, gbc_JTextFieldtipoDocumentoCliente);
+		
+	}
 
 }
